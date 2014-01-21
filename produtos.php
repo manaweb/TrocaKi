d608408a7f4593f209b32a2b30cfead204293296
wesleyholiveira <wesleyfetish@gmail.com>
2014-01-21 15:13:37 -0200

    Alteracoes realizadas e inserido pagina verprodutos

--- produtos.php
+++ produtos.php
@@ -26,7 +26,7 @@
           </h3>
           <ul class="bullet">
             <li>
-              <a href="javascript:void(0);">Componentes</a>
+              <a href="javascript:void(0);"></a>
             </li>
           </ul>
         </ul>
@@ -34,76 +34,22 @@
       <div id="produtos" class="clear produtos col-lg-8 col-md-8 co-sm-8 col-xs-8">
         <ul class="list-unstyled list-inline">
           <li class="produto">
-            <a href="verproduto.php?id=134">
-              <h3 class="titulo">Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS</h3>
+            <a href="verprodutos.php">
+              <h3 class="titulo">SUSPENSÃO</h3>
             </a>
-            <img src="http://rfcasaecia.com.br/painel/arquivos/produtos/_miniaturas/36b096e0151742be3e449f4b8e16bca7isabely.JPG" alt="Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS" class="">
+            <img src="img/suspensao.png" alt="SUSPENSÃO">
           </li>
           <li class="produto">
-            <a href="verproduto.php?id=134">
-              <h3 class="titulo">Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS</h3>
+            <a href="verprodutos.php?id=134">
+              <h3 class="titulo">FREIO</h3>
             </a>
-            <img src="http://rfcasaecia.com.br/painel/arquivos/produtos/_miniaturas/36b096e0151742be3e449f4b8e16bca7isabely.JPG" alt="Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS" class="">
+            <img src="img/freio.png" alt="FREIO">
           </li>
           <li class="produto">
-            <a href="verproduto.php?id=134">
-              <h3 class="titulo">Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS</h3>
+            <a href="verprodutos.php?id=134">
+              <h3 class="titulo">OLEO</h3>
             </a>
-            <img src="http://rfcasaecia.com.br/painel/arquivos/produtos/_miniaturas/36b096e0151742be3e449f4b8e16bca7isabely.JPG" alt="Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS" class="">
-          </li>
-          <li class="produto">
-            <a href="verproduto.php?id=134">
-              <h3 class="titulo">Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS</h3>
-            </a>
-            <img src="http://rfcasaecia.com.br/painel/arquivos/produtos/_miniaturas/36b096e0151742be3e449f4b8e16bca7isabely.JPG" alt="Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS" class="">
-          </li>
-          <li class="produto">
-            <a href="verproduto.php?id=134">
-              <h3 class="titulo">Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS</h3>
-            </a>
-            <img src="http://rfcasaecia.com.br/painel/arquivos/produtos/_miniaturas/36b096e0151742be3e449f4b8e16bca7isabely.JPG" alt="Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS" class="">
-          </li>
-          <li class="produto">
-            <a href="verproduto.php?id=134">
-              <h3 class="titulo">Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS</h3>
-            </a>
-            <img src="http://rfcasaecia.com.br/painel/arquivos/produtos/_miniaturas/36b096e0151742be3e449f4b8e16bca7isabely.JPG" alt="Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS" class="">
-          </li>
-          <li class="produto">
-            <a href="verproduto.php?id=134">
-              <h3 class="titulo">Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS</h3>
-            </a>
-            <img src="http://rfcasaecia.com.br/painel/arquivos/produtos/_miniaturas/36b096e0151742be3e449f4b8e16bca7isabely.JPG" alt="Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS" class="">
-          </li>
-          <li class="produto">
-            <a href="verproduto.php?id=134">
-              <h3 class="titulo">Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS</h3>
-            </a>
-            <img src="http://rfcasaecia.com.br/painel/arquivos/produtos/_miniaturas/36b096e0151742be3e449f4b8e16bca7isabely.JPG" alt="Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS" class="">
-          </li>
-          <li class="produto">
-            <a href="verproduto.php?id=134">
-              <h3 class="titulo">Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS</h3>
-            </a>
-            <img src="http://rfcasaecia.com.br/painel/arquivos/produtos/_miniaturas/36b096e0151742be3e449f4b8e16bca7isabely.JPG" alt="Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS" class="">
-          </li>
-          <li class="produto">
-            <a href="verproduto.php?id=134">
-              <h3 class="titulo">Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS</h3>
-            </a>
-            <img src="http://rfcasaecia.com.br/painel/arquivos/produtos/_miniaturas/36b096e0151742be3e449f4b8e16bca7isabely.JPG" alt="Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS" class="">
-          </li>
-          <li class="produto">
-            <a href="verproduto.php?id=134">
-              <h3 class="titulo">Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS</h3>
-            </a>
-            <img src="http://rfcasaecia.com.br/painel/arquivos/produtos/_miniaturas/36b096e0151742be3e449f4b8e16bca7isabely.JPG" alt="Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS" class="">
-          </li>
-          <li class="produto">
-            <a href="verproduto.php?id=134">
-              <h3 class="titulo">Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS</h3>
-            </a>
-            <img src="http://rfcasaecia.com.br/painel/arquivos/produtos/_miniaturas/36b096e0151742be3e449f4b8e16bca7isabely.JPG" alt="Kit ISABELY - 100% ALGODÃO PERCAL 200 FIOS" class="">
+            <img src="img/oleo.png" alt="OLEO">
           </li>
         </ul>
       </div>
---
+++ verprodutos.php
@@ -0,0 +1,168 @@
+    <?php include('header.php'); ?>
+
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+    <?php include('topo.php'); ?>
+    <link rel="stylesheet" href="css/jquery.bxslider.css" />
+    <div class="container">
+       <h2>PRODUTOS</h2>
+       <div id="corpoTodo" class="clearFix col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div id="fotoInformacoes" class="larguraTotal">
+          <div id="foto" class="aoLado">
+            <div id="zoom">
+              <img src="painel/arquivos/produtos/11bc8fe8a6b7ee0737bb939a722e58bfglen.JPG">
+            </div>
+            <div id="zoomhover" class="" style="background-image: url(http://rfcasaecia.com.br/painel/arquivos/produtos/11bc8fe8a6b7ee0737bb939a722e58bfglen.JPG); background-position: 10% 69%;"><img src="painel/arquivos/produtos/11bc8fe8a6b7ee0737bb939a722e58bfglen.JPG" style="display: none;"></div>
+            <div id="miniaturas">
+              <img src="painel/arquivos/produtos/_miniaturas/11bc8fe8a6b7ee0737bb939a722e58bfglen.JPG" name="img1" alt="Kit GLEN - 100% ALGODÃO PERCAL 200 FIOS"><img src="painel/arquivos/produtos/11bc8fe8a6b7ee0737bb939a722e58bfglen.JPG" id="img1" style="display: none;">       </div>
+          </div>
+          <div id="informacoes" class="aoLado">
+            <form action="cart.php?type=add" method="post">
+              <input type="hidden" name="id" value="129">
+              <input type="hidden" name="nome" value="Kit GLEN - 100% ALGODÃO PERCAL 200 FIOS">
+              <input type="hidden" name="foto1" value="11bc8fe8a6b7ee0737bb939a722e58bfglen.JPG">
+            <h3 class="corTitulo">Kit GLEN - 100% ALGODÃO PERCAL 200 FIOS</h3>
+            <div id="precos">
+
+            <div class="pull-right" style="margin-top: 15px;">
+              <span class="forma">Formas de Pagamento</span><br>
+              <span class="cartoes">Cartões de Crédito</span><br>
+                <img src="img/cielo.png" alt="Cielo">
+            </div>
+            <div class="clearfix"></div>
+            <div id="escolherQuantidade">
+
+              <div id="inputNumber">
+                <p>Escolha a quantidade</p>
+                <input type="text" id="qtd" name="qtd" value="1" readonly="">
+                <div id="controls">
+                  <div class="qtdMore"></div>
+                  <div class="qtdLess"></div>
+                </div>
+              </div>
+              <input type="submit" class="btnOrcamento" value="Orçamento">
+              
+            </div></form>
+          </div>
+        </div>
+        <div id="descricao" class="setorAbas">
+          <div class="topoAba">
+            <span class="aba">
+              DESCRIÇÃO DO PRODUTO
+            </span>
+          </div>
+          <div class="conteudoAba">
+            <p></p><div style="font-family: Arial, Helvetica, sans-serif !important; font-size: 13px; line-height: 1.5em !important;">Kit Solteiro 6 Peças</div><div style="font-family: Arial, Helvetica, sans-serif !important; font-size: 13px; line-height: 1.5em !important;">1 Colcha | 1 Porta Travesseiro</div><div style="font-family: Arial, Helvetica, sans-serif !important; font-size: 13px; line-height: 1.5em !important;">1 Almofada | 1 Refil Almofada</div><div style="font-family: Arial, Helvetica, sans-serif !important; font-size: 13px; line-height: 1.5em !important;">1 Rolo | 1 Refil Rolo</div><div style="font-family: Arial, Helvetica, sans-serif !important; font-size: 13px; line-height: 1.5em !important;"><br></div><div><font face="Arial, Helvetica, sans-serif"><span style="font-size: 12.727272033691406px; line-height: 19.488636016845703px;">Roupa de Cama Solteiro 3 Peças</span></font><br></div><p></p>
+          </div>
+          <div class="bottomAba">
+            <span class="voltarTopo">
+              VOLTAR AO TOPO
+            </span>
+          </div>
+        </div>
+
+        <div id="relacionados" class="setorAbas">
+          <div class="topoAba">
+            <span class="aba">
+              QUEM VIU, VIU TAMBÉM
+            </span>
+          </div>
+          <div class="abaProdutosRelacionados conteudoAba"><div class="produtoRelacionado">
+                <ul class="bxslider">
+                  <li>
+                    <a href="verproduto.php?id=92">
+                      <img src="painel/arquivos/produtos/_miniaturas/1e43695164658f22b8ce66f73ddc809apietro.JPG" alt="Cole��o Pietro - Percal 230 Fios - Xadrez Fio Tinto">
+                    </a>
+                    <a href="verproduto.php?id=92" class="saibaMais">Saiba Mais</a>
+                  </li>
+                  <li>
+                    <a href="verproduto.php?id=92">
+                      <img src="painel/arquivos/produtos/_miniaturas/1e43695164658f22b8ce66f73ddc809apietro.JPG" alt="Cole��o Pietro - Percal 230 Fios - Xadrez Fio Tinto">
+                    </a>
+                    <a href="verproduto.php?id=92" class="saibaMais">Saiba Mais</a>
+                  </li>
+                  <li>
+                    <a href="verproduto.php?id=92">
+                      <img src="painel/arquivos/produtos/_miniaturas/1e43695164658f22b8ce66f73ddc809apietro.JPG" alt="Cole��o Pietro - Percal 230 Fios - Xadrez Fio Tinto">
+                    </a>
+
+                    <a href="verproduto.php?id=92" class="saibaMais">Saiba Mais</a>
+                  </li>
+                  <li>
+                    <a href="verproduto.php?id=92">
+                      <img src="painel/arquivos/produtos/_miniaturas/1e43695164658f22b8ce66f73ddc809apietro.JPG" alt="Cole��o Pietro - Percal 230 Fios - Xadrez Fio Tinto">
+                    </a>
+         
+                    <a href="verproduto.php?id=92" class="saibaMais">Saiba Mais</a>
+                  </li>
+                  <li>
+                    <a href="verproduto.php?id=92">
+                      <img src="painel/arquivos/produtos/_miniaturas/1e43695164658f22b8ce66f73ddc809apietro.JPG" alt="Cole��o Pietro - Percal 230 Fios - Xadrez Fio Tinto">
+                    </a>
+            
+                    <a href="verproduto.php?id=92" class="saibaMais">Saiba Mais</a>
+                  </li>
+                </ul>
+              </div></div>
+          </div>
+          <div class="bottomAba">
+            <span class="voltarTopo">
+              VOLTAR AO TOPO
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    <?php include('rodape.php'); ?>
+    <script>
+        $(function(){
+
+          $(".qtdMore").click(function(){
+            var qtd = parseInt($("#qtd").val());
+            $("#qtd").val(qtd+1);
+          });
+
+          $(".qtdLess").click(function(){
+            var qtd = parseInt($("#qtd").val());
+              if(qtd > 1)
+                $("#qtd").val(qtd-1);
+          });
+
+          $(".voltarTopo").click(function(){
+            $("html, body").animate({ scrollTop: 0 }, 600);
+            return false;
+          });
+
+          $("select").change(function(){
+            $(this).css('background-color', "#" + $(this).val());
+            if($(this).val() == 'Selecione a Cor'){
+              $(this).css('background-color', "#FFFFFF");
+            }
+          })
+
+          $("#miniaturas img").hover(function(){
+            var id = $(this).attr('name');
+            var imagem = $("#"+id).attr('src');
+            $("#zoom img, #zoomhover img").attr('src',imagem);
+            $("#zoomhover").css("background-image","url("+imagem+")");
+          });
+
+        });
+      </script>
+      <script src="js/jquery.bxslider.min.js"></script>
+    <script>
+    $(function(){
+      $('.bxslider').bxSlider({
+        minSlides: 4,
+        maxSlides: 4,
+        slideWidth: 190,
+        slideMargin: 10
+
+      });
+    })
+    </script>
+    </body>
+</html>
